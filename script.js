03790ec033454e4107002815353fdb1c98d4d5b1
Daniel O <danielogunlana23@gmail.com>
2021-05-31 04:44:05 -0400

    made css and javascript start function

---
+++ script.js
@@ -0,0 +1,21 @@
+var startButton = document.getElementById("startBtn")
+
+
+startButton.addEventListener("click", startGame)
+
+function startGame () {
+
+}
+
+var questions = [
+  {
+    question: "What is Javascript",
+    answers: [
+      {text: "1", correct:false}
+      {text: "1", correct:false}
+      {text: "1", correct:false}
+      {text: "1", correct:true}
+    ]
+  
+}]
+
