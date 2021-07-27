e22de53dacecf6e84d0d4bdb86062bbae1233772
Daniel O <danielogunlana23@gmail.com>
2021-07-27 04:06:54 -0400

    added function to display questions and answers

--- game.js
+++ game.js
@@ -1,31 +1,125 @@
-var questionEl = document.querySelector("#questions")
-
+var questionEl = document.querySelector("#questions");
+var btnEl = document.querySelector("#startBtn");
+var startContainer = document.querySelector("#start-container");
+var gameContainer = document.querySelector("#game");
 var questions = [
+	{
+		question: "What is Javascript",
+		answers: [
+			{ text: "1", correct: false },
+			{ text: "2", correct: false },
+			{ text: "3", correct: false },
+			{ text: "4", correct: true },
+		],
+	},
   {
-    question: "What is Javascript",
-    answers: [
-      {text: "1", correct:false},
-      {text: "2", correct:false},
-      {text: "3", correct:false},
-      {text: "4", correct:true}
-    ]
-  
-}]
+		question: "What is Javascript",
+		answers: [
+			{ text: "9", correct: false },
+			{ text: "6", correct: true },
+			{ text: "3", correct: false },
+			{ text: "5", correct: false },
+		],
+	},
+  {
+		question: "What is Javascript",
+		answers: [
+			{ text: "9", correct: false },
+			{ text: "6", correct: true },
+			{ text: "3", correct: false },
+			{ text: "5", correct: false },
+		],
+	},
+  {
+		question: "What is Javascript",
+		answers: [
+			{ text: "9", correct: false },
+			{ text: "6", correct: true },
+			{ text: "3", correct: false },
+			{ text: "5", correct: false },
+		],
+	},
+  {
+		question: "What is Javascript",
+		answers: [
+			{ text: "9", correct: false },
+			{ text: "6", correct: true },
+			{ text: "3", correct: false },
+			{ text: "5", correct: false },
+		],
+	},
+];
 
-questions[0].question
-questions[0].answers
-for (i=0; i<questions[0].answers.length; i++) {
-questions[0].answers[i]
-}
+// make a function to startgame then timer starts and questions show up on screen
+
+// function timer()
+
+// after displayQandA function run an addEventlistener to look for the user click
+
+// when they start the quiz a timer should start and they should be presented with the first question
+// If they get a question wrong they should be deducted time and it would be their quiz score
+//game should end when all questions are answered or the timer reaches zero
+// when they are done they should get their score  and should be able to save it by entering their initials
+// a go back button should return them to the home page
+
+let currentIndex = 0
 
 function displayQandA() {
-  console.log("hello")
-  var question = questions[0].question;
-  questionEl.textContent=question;
-  var answers = questions[0].answers
-  answers.forEach(function(answer, index) {
-    document.getElementById(index.toString()).textContent = answer.text
-  }) 
+	startContainer.setAttribute("style", "visibility:hidden; display:none;");
+	gameContainer.setAttribute("style", "visibility:visible; display:inline;");
+	console.log("hello");
+	var question = questions[currentIndex].question;
+	questionEl.textContent = question;
+	var answers = questions[currentIndex].answers;
+	answers.forEach(function (answer, index) {
+		document.getElementById(index.toString()).textContent = answer.text;
+		document.getElementById(index.toString()).onclick = checkAnswer;
+	});
 }
 
-displayQandA()
+function checkAnswer(e) {
+	console.log(this.textContent);
+	questions[currentIndex].answers.forEach((answer) => {
+		if (answer.correct) {
+			if (answer.text === this.textContent) {
+				console.log("Correct");
+			} else {
+				console.log("wrong");
+			}
+		}
+	});
+  if (currentIndex === questions.length - 1) {
+    console.log("End of quiz")
+  }else {
+    currentIndex ++
+    displayQandA()
+  }
+}
+
+btnEl.addEventListener("click", function() {
+  displayQandA()
+  
+})
+// btnEl.addEventListener("click", function(event) {
+//   event.preventDefault()
+//   var correctChoice = event.target;
+
+//   if (correctChoice.matches(answers.correct === true)) {
+
+//     // if (answers.correct) {
+//       questions.question++
+
+//     var body = document.getElementsByClassName('question-Container')[0],
+//     choiceReply = document.createElement('h2');
+//     h2.textContent = "Correct!"
+//     choiceReply.id = 'newid';
+//     question-Container.appendChild(choiceReply);
+
+//   var
+//   var choiceReply = document.createElement('h3')
+//   .body.appendChild(choiceReply)
+//   choiceReply.textContent("Correct!")
+//   console.log(choiceReply)
+// }
+//   }
+// })
